37ec1dc57f24aa448392220babf94450b03562f8
Vitor Rangel <rangel.final@gmail.com>
2019-04-02 15:16:15 -0300

    Chore: Remove unused option

--- lib/EntityDatabase.ts
+++ lib/EntityDatabase.ts
@@ -12,7 +12,6 @@ export interface EntityDatabaseOptions extends DatabaseOptions {
   connection?: Connection;
   connectionOpts?: ConnectionOptions;
   customQueriesDir?: string;
-  entities: any[];
 }
 
 export default class EntityDatabase extends Database {
